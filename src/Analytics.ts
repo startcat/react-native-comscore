c4ccb5e6741c4fd3a05e5db73f8f123e8eba2e33
Daniel Marín <daniel.marin@start.cat>
2024-08-06 14:12:31 +0200

    Init

---
+++ src/Analytics.ts
@@ -0,0 +1,65 @@
+import { Platform, DeviceEventEmitter, NativeEventEmitter, NativeModules } from 'react-native';
+import { EventRegister } from 'react-native-event-listeners';
+
+type Config = {
+    publisherId: string;
+    applicationName: string;
+};
+
+
+
+/*
+ *  Comscore Module
+ *  Interface for the Comscore Analytics module
+ * 
+ */
+
+const { RNComscoreModule } = NativeModules;
+
+class Singleton {
+
+    static #instance: Singleton;
+
+    private log_key: string = `[Downloads]`;
+    private initialized: boolean = false;
+
+    private constructor() { }
+
+    public static get instance(): Singleton {
+
+        if (!Singleton.#instance) {
+            Singleton.#instance = new Singleton();
+        }
+
+        return Singleton.#instance;
+
+    }
+
+    private log (message:string): void {
+        console.log(`${this.log_key} ${message}`);
+    }
+
+    public init (config:Config): Promise<void> {
+
+        return new Promise((resolve, reject) => {
+
+            this.log(`Config Settings: ${JSON.stringify(config)}`);
+            return resolve();
+
+        });
+
+    }
+
+	public trackView (view:string): void {
+		RNComscoreModule.trackView(view);
+
+	}
+
+	public trackEvent(action:string, category:string): void {
+		RNComscoreModule.trackEvent(action, category);
+
+	}
+
+}
+
+export default Singleton.instance;
