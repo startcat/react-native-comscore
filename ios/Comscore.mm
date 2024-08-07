fe4f426ab0bcbc659eff915fc99e8bff0652c900
Daniel Marín <daniel.marin@start.cat>
2024-08-07 15:04:35 +0200

    feat(danimarin): add dependencies

--- ios/Comscore.mm
+++ ios/Comscore.mm
@@ -6,6 +6,10 @@ RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
 
+RCT_EXPORT_METHOD(trackView:(NSString *) view)
+
+RCT_EXPORT_METHOD(trackEvent:(NSString *)action category:(NSString *)category)
+
 + (BOOL)requiresMainQueueSetup
 {
   return NO;
--- ios/Comscore.swift
+++ ios/Comscore.swift
@@ -1,8 +1,23 @@
+import Foundation
+import React
+
 @objc(Comscore)
 class Comscore: NSObject {
 
   @objc(multiply:withB:withResolver:withRejecter:)
   func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
     resolve(a*b)
+  }
+
+  @objc(trackView:)
+  func trackView(_ view:String) -> Void {
+    RCTLog("+++ [ComScore] trackView");
+    
+  }
+
+  @objc(trackEvent:category:)
+  func trackEvent(_ action:String, category:String) -> Void {
+    RCTLog("+++ [ComScore] trackEvent");
+    
   }
 }
