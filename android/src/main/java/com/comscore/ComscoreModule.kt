c6126cc627dbe3e32846cf3b4296d457fa1afaa4
Daniel Marín <daniel.marin@start.cat>
2024-08-07 12:53:05 +0200

    feat(danimarin): add dependencies

--- android/src/main/java/com/comscore/ComscoreModule.kt
+++ android/src/main/java/com/comscore/ComscoreModule.kt
@@ -5,6 +5,13 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
 
+import java.util.List
+import java.util.Map
+import java.util.HashMap
+
+import com.comscore.Analytics
+import com.comscore.PublisherConfiguration
+
 class ComscoreModule(reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
 
@@ -18,6 +25,23 @@ class ComscoreModule(reactContext: ReactApplicationContext) :
   fun multiply(a: Double, b: Double, promise: Promise) {
     promise.resolve(a * b)
   }
+
+  @ReactMethod
+	public void trackView(String view) {
+
+		HashMap<String,String> labels = new HashMap<String,String>();
+		labels.put("name", view.replace("/", "."));
+		Analytics.notifyViewEvent(labels);
+
+	}
+
+	@ReactMethod
+	public void trackEvent(String action, String category) {
+		String comScoreEventName = category + "." + action;
+		HashMap<String,String> labels = new HashMap<String,String>();
+		labels.put("event", comScoreEventName);
+		Analytics.notifyViewEvent(labels);
+	}
 
   companion object {
     const val NAME = "Comscore"
